30b2855777e5d29639710bde53485de6571a4bfb
JRook98 <rookja10@rowan.edu>
2026-09-26 01:36:02 -0400

    Add files via upload

---
+++ NjvftApp/includes/footer.php
@@ -0,0 +1,4 @@
+<!-- Footer -->
+<footer>
+    <p>&copy; 2026 NJVFT: New Jersey Virtual Field Trip. CS10430. All rights reserved.</p>
+</footer>
---
+++ NjvftApp/includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>NJVFT</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+ <!-- Title of website as header -->
+    <header>
+        <img src="Insert_Logo.png" alt="NJVFT_Logo">
+        <div class="header-text">
+            <h1>NJVFT</h1>
+            <h3>New Jersey Virtual Field Trip</h3>
+        </div>
+    </header>
